1428805912c688b44914dd60b11fd7136d15817e
kunal062003 <102716904+kunal062003@users.noreply.github.com>
2022-06-11 00:02:12 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,27 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "realme 8i",
+    desc : "Greatest properly off ham exercise all.",
+    img : "p1.png",
+    price :13999
+  },
+  124: {
+    name : "realme 8 5G",
+    desc : "Unsatiable its possession nor off.",
+    img : "p2.png",
+    price : 16999
+  },
+  125: {
+    name : "Xiaomi 11T 5G",
+    desc : "All difficulty unreserved the solicitude.",
+    img : "p3.png",
+    price : 15999
+  },
+  126: {
+    name : "Xiaomi 11i",
+    desc : "Had judgment out property the supplied. ",
+    img : "p4.png",
+    price : 22500
+  }
+};
